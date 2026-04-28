eba9ce6e80bb02cfa93a696c702394f6c624dbb4
Udit <dakuaudit@gmail.com>
2026-04-28 14:48:58 +0530

    fix: update next.js and fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,4 @@
-// import { headers } from "next/headers";
-import { detectBot, shield } from "@arcjet/next";
 import { NextRequest, NextResponse } from "next/server";
-import aj from "./lib/arcjet";
-import { createMiddleware } from "@arcjet/next";
-// import { auth } from "./lib/auth";
-
-
 
 export async function middleware(request: NextRequest) {
   const sessionCookie = request.cookies.get("better-auth.session_token");
@@ -16,11 +9,7 @@ export async function middleware(request: NextRequest) {
 
   return NextResponse.next();
 }
-const validate = aj 
-.withRule(shield({mode:'LIVE'}))
-.withRule(detectBot({mode:'LIVE',allow:['CATEGORY:SEARCH_ENGINE' , "GOOGLE_CRAWLER"] }))
 
-export default createMiddleware(validate)
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)'],
 };
